240ea8efcd5ed3cabd056d245e4a3344114fc260
Mateus Gomes da Silva <mateus.gomes@estudante.ufjf.br>
2025-10-20 20:50:25 -0300

    Criando as funções inicias do tabuleiro

--- src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/tabuleiro.java
+++ src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/tabuleiro.java
@@ -1,5 +1,37 @@
 package ufjf.dcc025.batalhadewesteros.Secundario;
 
+import ufjf.dcc025.batalhadewesteros.Personagens.personagem;
+import java.util.Scanner;
+
 public class tabuleiro {
     
+    ///função para verificar se a posicao do personagem está dentro do tamanho do tabuleiro
+    public boolean dentroLimite(int linha, int coluna){
+
+    }
+    
+    ///função para colocar o personagem na posicao, caso ela esteja livre
+    public boolean posicicao(personagem p, int linha, int coluna){
+
+    }
+
+    ///funcao para verificar se a posicao está vazia
+    public boolean verVazio(int linha, int coluna){
+
+    }
+
+    ///funcao para fazer a movimentacao do personagem(W,A,S,D) 
+    public void movimentacao(personagem p){
+
+    }
+
+    ///funcao para imprimir a situacao do tabuleiro no console
+    public void imprimirTabuleiro(){
+
+    }
+
+    ///funcao para limpar o console antes de imprimir novamente
+    public static void limparTabuleiro(){
+
+    }
 }
