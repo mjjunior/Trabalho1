4b702a7a5a2862bd58ad2ca9c3dfc9b8bb1bd6e6
junior <mjjunior360@gmail.com>
2025-10-20 17:05:06 -0300

    criando interface personagem e os personagens da da classe stark

--- src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/stark.java
+++ src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/stark.java
@@ -3,14 +3,64 @@ import  ufjf.dcc025.batalhadewesteros.personagem;
 
 public class stark implements personagem{
 
+    
+    private String nome;
+    private int vidaMaxima = 60;
+    private int vidaAtual;
+    private int alcanceMaximo = 1; 
+    
+    public stark(String nome){
+        this.nome = nome;
+        vidaAtual = vidaMaxima;
+    }
+    
+    
     @Override
     public void atacar(personagem alvo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        /// Chamar menu inrormando o ataque e exibindo as informações;
+        alvo.recebeDano(ATAQUE_BASE);
     }
 
     @Override
-    public void defender() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void recebeDano(float dano) {
+        
+        // Opção 
+        
+        /* 
+        float danoReal = dano - DEFESA_BASE;
+        float modificadorDefensivo = dano * 0.2f;
+        vidaAtual -= danoReal - modificadorDefensivo; 
+        */
+        vidaAtual -= (dano - DEFESA_BASE) * 0.8f;
+        
+        ///chamar aqui um menu passando a vida restante e o dano recebido
+        
     }
-    
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public int getVidaAtual() {
+        return vidaAtual;
+    }
+
+    @Override
+    public int getVidaMaxima() {
+        return vidaMaxima;
+    }
+
+    @Override
+    public int getAlcance() {
+        return alcanceMaximo;
+    }
+
+    @Override
+    public String getTipo() {
+        return "STARK";
+    }
+   
 }
--- src/main/java/ufjf/dcc025/batalhadewesteros/personagem.java
+++ src/main/java/ufjf/dcc025/batalhadewesteros/personagem.java
@@ -2,6 +2,19 @@ package ufjf.dcc025.batalhadewesteros;
 
 public interface personagem {
     
+   public static final int ATAQUE_BASE = 20;
+   public static final int DEFESA_BASE = 10;
+   
+   
+    
     void atacar(personagem alvo);
-    void defender();
+    void recebeDano(int dano);
+    
+    String getNome();
+    int getVidaAtual();
+    int getVidaMaxima();
+    int getAlcance();
+    
+    String getTipo();
+    
 }
