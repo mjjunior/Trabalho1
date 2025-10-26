56e677029ae6940c32e33c4250010077b73fc85a
Sávio Machado Lima <saviomachadolima2003@gmail.com>
2025-10-26 15:12:40 -0300

    Finaliza (espero eu) correção dos erros de sincronização

--- src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/bot.java
+++ src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/bot.java
@@ -1,5 +1,34 @@
 package ufjf.dcc025.batalhadewesteros.Secundario;
 
-public class bot {             
+import java.util.ArrayList;
+import java.util.List;
+import ufjf.dcc025.batalhadewesteros.Secundario.Personagens.linnister;
+import ufjf.dcc025.batalhadewesteros.Secundario.Personagens.personagem;
+import ufjf.dcc025.batalhadewesteros.Secundario.Personagens.stark;
+import ufjf.dcc025.batalhadewesteros.Secundario.Personagens.targaryen;
+
+public class bot {       
     
+    /// Crriei apenas para o teste e configuracao do replay
+   private static List<personagem> timeBot;
+    
+    public bot(){
+        
+        timeBot = new ArrayList<>();
+        
+        stark p1 = new stark("BOT: stark");
+        linnister p2 = new linnister("BOT: linnister");
+        targaryen p3 = new targaryen("BOT: targaryen");
+        
+        timeBot.add(p1);
+        timeBot.add(p2);
+        timeBot.add(p3);
+    }
+    
+    
+    ///
+    ///Seria interessante usarmos um metodo statico pois nao precisaremos instanciar
+    ///um objeto do tipo bot para ultilizar seus metodos
+    /// 
+    public static List<personagem> getPersonagem(){return timeBot;} 
 }
