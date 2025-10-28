2c4bdb2faaabb3831e9fdde713fd1de7898d1d65
junior <mjjunior360@gmail.com>
2025-10-28 17:49:07 -0300

    ajustando classe replay

--- src/main/java/ufjf/dcc025/batalhadewesteros/Interface/replay.java
+++ src/main/java/ufjf/dcc025/batalhadewesteros/Interface/replay.java
@@ -13,18 +13,6 @@ public class replay {
         replayPartida = new ArrayList<>();
     }
     
-    ///
-    /// OBS:
-    /// talvez aqui possamos simplificar para nao ter 2 modos diferentes
-    /// podemos ter apenas um modo jogo , onde ele recebe os parametros
-    /// como se fosse no modo multiplayer (lista pq, lista p2) e tratamos
-    /// essas listas na chamada , tipo , se o modo escolhido foi o single-player
-    /// o segundo parametro passado será a lista de personagens do bot
-    /// caso multiplayer enão seguimos normal
- 
-    /// @param rodada
-    /// salva a jogada da rodada
-    ///
     public void salvar(String rodada){
         replayPartida.add(rodada);
     }
