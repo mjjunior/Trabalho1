019406e79047722348f2078eb840f71b9e1dc8cf
Mateus Gomes da Silva <mateus.gomes@estudante.ufjf.br>
2025-10-28 09:15:33 -0300

    pensando em uma possivel implementação do bot

--- src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/bot.java
+++ src/main/java/ufjf/dcc025/batalhadewesteros/Secundario/bot.java
@@ -31,4 +31,43 @@ public class bot {
     ///um objeto do tipo bot para ultilizar seus metodos
     /// 
     public static List<personagem> getPersonagem(){return timeBot;} 
+
+    ///Mateus:
+    ///gente pensei em um possivel  metodo
+    ///a gente pode sortear uma casa aleatoria usando random e criar 3 personagens dessa casa, depois posiciona o time do bot automatico e faz ele se mover e atacar sozinho
+    /// tipo assim:
+    /// imoport java.util.Random;
+    /// public class bot{
+    ///     private ArrayList<personagem> timeDoBot;
+    ///     private String casaDoBot;
+    /// }
+    /// public bot(){
+    ///     this.timeDoBot = new ArrayList<>();
+    ///     this.casaDoBot = casaAleatoria();
+    ///     inicializaTimeDoBot();
+    /// }
+    /// ///sorteando a casa do bot
+    ///  private String casaAleatoria(){
+    ///     Random rand = new Random();
+    ///     int escolher = rand.nextInt(3);
+    ///     switch(escolha){
+    ///          case 0: 
+    ///                return "Stark";
+    ///                break;
+    ///          case 1:
+    ///                return "Linnister";
+    ///                break;
+    ///          default:
+    ///               return "Targaryen";
+    ///     }
+    /// }
+    /// aí pensa numa função para inicializar o time do bot, que ainda n pensei kkkk
+    /// aqui alguns getters para acessar fora da classe
+    /// public ArrayList<personagem> getTimeDoBot(){
+    ///     return timeDoBot;
+    /// }
+    /// public String getCasaDoBot(){
+    ///     return casaDoBot;
+    /// }
+    /// e ate tentar fazer em algo para mostrar as informação do time
 }
