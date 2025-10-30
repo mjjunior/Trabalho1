388ca7da66e5c600f6c7d03ae3899e15974fb05e
Sávio Machado Lima <saviomachadolima2003@gmail.com>
2025-10-30 12:07:25 -0300

    Limpa console ao iniciar o programa

--- src/main/java/ufjf/dcc025/batalhadewesteros/BatalhaDeWesteros.java
+++ src/main/java/ufjf/dcc025/batalhadewesteros/BatalhaDeWesteros.java
@@ -10,6 +10,18 @@ public class BatalhaDeWesteros {
         menu jogo = new menu();
         JOptionPane.showMessageDialog(null, "Bem vindo a Batalha Tática das Casas de  Westeros! \n" + 
                                     "Monte um time com 3 personagens e batalhe contra um amigo ou teste suas habilidades de maneira solo.");
+
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();      //limpa o console
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            for (int i = 0; i < 50; i++) System.out.println();
+        }
+        
         jogo.menuPrincipal();
     }   
 }
